29bc4509274303038622451f527b9f8e1021081f
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-08-11 14:49:11 +0200

    2. házi kész

---
+++ test_phyton/test_registration.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+# In order for ChromeDriverManager to work you must pip install it in your own environment.
+
+def test_registration():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    URL = "http://localhost:1667/"
+    options = Options()
+    options.headless = True
+
+    driver.get(URL)
+
+    link=driver.find_element_by_xpath('//li[@class="nav-item"]/a[@href="#/register"]')
+
+    link.click()
+    assert 1==1
+    driver.close()
+

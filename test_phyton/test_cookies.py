68bbf14beb5080941f533a6b79379e4bf0c42fc5
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-08-16 14:12:51 +0200

    cookivak kapcsolatos ellenőrzések

--- test_phyton/test_cookies.py
+++ test_phyton/test_cookies.py
@@ -10,11 +10,22 @@ import pages.func as f
 def test_cookies():
     p_main = m.PageMain()
     p_main.open()
+
+    #ha nincs cookie, akkor láthatónak kellene lennie, ez időnként elbukik
+    if p_main.getcookies() == "":
+        assert p_main.cookies_button_visible() is True
+
     #törli a cookiekat
     p_main.deletecookies()
     p_main.open()
     # gombok láthatóak-e
     assert p_main.cookies_button_visible() is True
+    p_main.cookies_website_click()
+
+    #jó oldalra visz e a cookies link
+    assert p_main.get_and_close_second_window_url() == "https://www.cookiesandyou.com/"
+
+    time.sleep(20)
     p_main.cookies_accept()
     assert p_main.getcookies() == 'accept'
 
@@ -29,4 +40,3 @@ def test_cookies():
     p_main.cookies_decline()
     assert p_main.getcookies() == 'decline'
 
-test_cookies()

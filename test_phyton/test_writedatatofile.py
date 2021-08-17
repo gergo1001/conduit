46f632183f7e9b6b36270a2fd19e7da290d60361
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-08-17 20:15:03 +0200

    kész

--- test_phyton/test_writedatatofile.py
+++ test_phyton/test_writedatatofile.py
@@ -4,16 +4,14 @@ import pages.page_main as m
 def test_datawritetofile():
     p_main = m.PageMain()
     p_main.open()
-    try:
-        p_signin = p_main.signinpage_open()
-        # tesztadatok
-        testemail = 'testuser1@example.com'
-        testpassw = 'Abcd123$'
-        testuser = 'testuser1'
-        # megadott teszt userrel belépés
-        p_signin.fill_inputs(testemail, testpassw)
-        p_signin.click_signin()
-        p_main.articles_write_to_file('./test_phyton/output.csv')
-    finally:
-        p_main.__del__()
+    p_signin = p_main.signinpage_open()
+    # tesztadatok
+    testemail = 'testuser1@example.com'
+    testpassw = 'Abcd123$'
+    testuser = 'testuser1'
+    # megadott teszt userrel belépés
+    p_signin.fill_inputs(testemail, testpassw)
+    p_signin.click_signin()
+    p_main.articles_write_to_file('./test_phyton/output.csv')
+
 

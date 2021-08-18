aee24406904843bbd102935751ae984ac6ddbc77
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-08-18 12:44:46 +0200

    kész

--- test_phyton/test_inputdata.py
+++ test_phyton/test_inputdata.py
@@ -2,7 +2,6 @@ import csv
 import pages.page_main as m
 
 
-
 def test_inputdata():
     p_main = m.PageMain()
     p_main.open()
@@ -15,7 +14,7 @@ def test_inputdata():
         # megadott teszt userrel belépés
         p_signin.fill_inputs(testemail, testpassw)
         p_signin.click_signin()
-        tittles=[]
+        tittles = []
         with open('./test_phyton/input.csv', newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter='\t')
             next(reader)
@@ -25,13 +24,19 @@ def test_inputdata():
                 p_editor.fill_inputs(row[0], row[1], row[2], row[3])
                 p_editor.click_publish()
         p_editor.go_home()
-        upload_element_count=len(tittles)
-        checked_upload=0
+        upload_element_count = len(tittles)
+        checked_upload = 0
         for tittle in tittles:
             if p_main.search_article_with_title(tittle):
                 checked_upload += 1
-        #ellenőrzi, hogy mindegyik feltöltésre került
+        # ellenőrzi, hogy mindegyik feltöltésre került
         assert upload_element_count == checked_upload
+
+        # kitörtli a felvitt adatokat
+        for tittle in tittles:
+            p_main.del_article(tittle, False)
     finally:
         p_main.__del__()
-test_inputdata()
+
+
+
